14ba84117e17577573fb3d5e61161564a99513b9
Almazq <almazbidullaev@gmail.com>
2021-12-03 18:01:23 +0600

    weather Kazahstan version design

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// fetch('http://api.weatherapi.com/v1/current.json?key=a4d4b786e5244b80bc7184032212511&q=Atyrau&aqi=no')
+// 			.then(function(resp){return resp.json()})
+// 			.then(function(date)
+// 				{
+// 					console.log(date);
+// 					city.innerHTML = date.location.region;
+// 					time.innerHTML = date.location.localtime;
+// 					temp.innerHTML = date.current.temp_c;
+// 					wind.innerHTML = date.current.wind_kph;
+// 					humidity.innerHTML = date.current.humidity;
+
+// 				})
